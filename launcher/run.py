82c61db97c4b726cdfac0af2dc513755335978be
haiqwa <haiqwa>
2020-08-02 14:38:42 +0800

    add ortools module to requirment

--- launcher/run.py
+++ launcher/run.py
@@ -2,14 +2,14 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description="launch the training script")
-parser.add_argument("--data_url", required=True,default='/home/haiqwa/dataset/cifar10',help="the bucket path of dataset")
+parser.add_argument("--data_url",default='/home/haiqwa/dataset/cifar10',help="the bucket path of dataset")
 parser.add_argument("--train_url",default=None,help="the output file stored in")
-parser.add_argument("--ckpt_path",default=None,help="the root directory path of model checkpoint stored in")
+parser.add_argument("--ckpt_path",default='./ckpt',help="the root directory path of model checkpoint stored in")
 parser.add_argument("--num_gpus",required=True,type=int,default=1,help="the num of devices used to train")
 
 args_opt,_ = parser.parse_known_args()
 
-models = ['resnet18','resnet34','resnet50','resnet101','resnet152','vgg11','vgg13','vgg16','vgg19']
+models = ['resnet152']
 
 
 for model in models:
@@ -19,7 +19,8 @@ for model in models:
 	batch_size = 64
 	num_gpus = args_opt.num_gpus
 	class_num = 10
-	mesh_shape = 'b1:2\\;b2:2'
+	# mesh_shape = 'b1:2\\;b2:2'
+	mesh_shape = 'b1:2'
 	cmd = "python adsl4mtf/launcher/main.py \
 				--data_url={} \
 				--ckpt_path={} \
