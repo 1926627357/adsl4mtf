98fb630067a7cfbe9f73e5488bc2a264d6300fd2
haiqwa <1926627357@qq.com>
2020-08-17 15:05:38 +0800

    optimize the drawer script

--- utils/124gpu.py
+++ utils/124gpu.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import csv
 
-num_classes = [10,1024,65536,131072]
+num_classes = [10,1000,10000,65536]
 use_fp16 = [0,1]
 parallel = ["AUTO_PARALLEL"]
 device_num = [1,2,4,8]
@@ -19,8 +19,8 @@ def buildfilename(modelname):
     strlist = []
     for a in range(4):
         for b in range(2):
-            for c in range(2):
-                strtmp = "model_" + modelname + "_num_classes_"+ str(num_classes[a]) + "_use_fp16_" + str(use_fp16[b]) + "_batch_size_32_parallel_mode_" + str(parallel[c]) +"_epoch_size_5_device_num_"
+            for c in range(1):
+                strtmp = "model_" + modelname + "_num_classes_"+ str(num_classes[a]) + "_use_fp16_" + str(use_fp16[b]) + "_batch_size_32.0_parallel_mode_" + str(parallel[c]) +"_epoch_size_5_device_num_"
                 strlist.append(strtmp)
     return strlist
 
@@ -42,16 +42,19 @@ def getdata(f,dtmp):
             with open(fcsv, 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
                 i = 1
+                mean_speed = 0
+                count = 0
                 for row in reader:
-                    if i == 3:
-                        ylist.append(float(row['samples/second']))
+                    if i >= 3:
+                        mean_speed+=float(row['samples/second'])
+                        count+=1
                         # print("[start]")
                         # print(fcsv)
                         # print(ylist)
                         # print()
 
                     i = i + 1
-
+                ylist.append(mean_speed/count)
     return ylist,ylen
 
 def getLinear(t):
@@ -74,13 +77,25 @@ def drawbar(modelname):
     plt.figure(modelname, (8, 8))
     fnamelist = buildfilename(modelname)
     pnum = 1
+
+    y_limitation = 1000
+    def autolabel(rects):
+        for rect in rects:
+            height = rects:
+            height = rect.get_height()
+            plt.annotate('{}'.format(int(height)),
+                            xy=(rect.get_x()+rect.get_width()/2,height if height<y_limitation-200 else y_limitation/2),
+                            xytext=(0,3),
+                            textcoords="offset points",
+                            ha="center",va="bottom",fontsize=8)
+
     for subpicname in fnamelist:
         y1 , y1len = getdata(subpicname,dir1)
         # y2 , y2len = getdata(subpicname,dir2)
         linearbar = getLinear(y1[0])
         # if y1len or y2len:
         if y1len:
-            plt.subplot(subnum, subnum, pnum)
+            plt.subplot(4, 2, pnum)
             plt.subplots_adjust(left=0.06, top=0.92, right=0.96, bottom=0.04, wspace=0.55, hspace=0.55)
             plt.ylabel('samples/second', fontsize=6, labelpad=1)
             plt.xlabel('device_num', fontsize=6, labelpad=0.5)
@@ -89,10 +104,10 @@ def drawbar(modelname):
             plt.title(gettitle(subpicname), fontsize=6)
 
             # plt.yscale('symlog')
-            # plt.ylim(0, 100000)
+            plt.ylim(0, y_limitation)
 
             x = list(range(len(y1)))
-            total_width, n = 0.4, 2
+            total_width, n = 0.8, 2
             width = total_width / n
 
             # draw 1
@@ -109,11 +124,11 @@ def drawbar(modelname):
             plt.bar(x, linearbar, width=width, label='o-linear', tick_label=y1, fc='#2B60DE')
 
             plt.xticks((0, 1, 2, 3), ('1', '2', '4', '8'))
-            plt.legend(loc='upper left', fontsize=4)
+            plt.legend(loc='upper left', fontsize=8)
             pnum += 1
 
     plt.suptitle(modelname + "_bactch_size_32_epoch_size_3", fontsize=8)
-    plt.savefig("picture/" + modelname + "_bactch_size_32_epoch_size_3.png", format="png")
+    plt.savefig("picture/" + modelname + "_bactch_size_32_epoch_size_5.pdf", format="pdf")
    # plt.show()
 
 
