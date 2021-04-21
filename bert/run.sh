e2ccab88988c12be5321f9337f02b327842eaba5
haiqwa <1926627357@qq.com>
2021-04-21 16:55:54 +0800

    update training data address

--- bert/run.sh
+++ bert/run.sh
@@ -1,7 +1,10 @@
 rm -rf output/
-python run_pretraining.py --mode=data_parallel --bert_config_file=./bert_config.json --input_train_files=/data/DNN_Dataset/dataset/zhwiki/tfdir/ --output_dir=./output --gpu_num=2 &> 2.log
+python run_pretraining.py --mode=data_parallel --bert_config_file=./bert_config.json --input_train_files=/data/DNN_Dataset/zhwiki/tfdir/ --output_dir=./output --gpu_num=2 &> 2.log
+echo "gpu-2 done"
 rm -rf output/
-python run_pretraining.py --mode=data_parallel --bert_config_file=./bert_config.json --input_train_files=/data/DNN_Dataset/dataset/zhwiki/tfdir/ --output_dir=./output --gpu_num=1 &> 1.log
+python run_pretraining.py --mode=data_parallel --bert_config_file=./bert_config.json --input_train_files=/data/DNN_Dataset/zhwiki/tfdir/ --output_dir=./output --gpu_num=1 &> 1.log
+echo "gpu-1 done"
 rm -rf output/
-python run_pretraining.py --mode=data_parallel --bert_config_file=./bert_config.json --input_train_files=/data/DNN_Dataset/dataset/zhwiki/tfdir/ --output_dir=./output --gpu_num=4 &> 4.log
+python run_pretraining.py --mode=data_parallel --bert_config_file=./bert_config.json --input_train_files=/data/DNN_Dataset/zhwiki/tfdir/ --output_dir=./output --gpu_num=4 &> 4.log
+echo "gpu-4 done"
 rm -rf output/
